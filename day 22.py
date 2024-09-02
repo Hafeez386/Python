872362cd037e9a0b2121ed554fea852a52445fbe
hafee@DESKTOP-C5J7INP <hafeez2713@gmail.com>
2024-09-02 23:17:31 +0500

    Build Pong_The Famous Arcade Game

---
+++ day 22.py
@@ -0,0 +1,121 @@
+import time
+from turtle import Screen, Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+
+def main():
+    screen = Screen()
+    screen.setup(width=800, height=600)
+    screen.title("Pong")
+    screen.bgcolor("black")
+    screen.tracer(0)
+
+    r_paddle = Paddle((350, 0))
+    l_paddle = Paddle((-350, 0))
+    ball = Ball()
+    score = Score()
+
+    screen.listen()
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "x")
+
+    is_game_on = True
+
+    while is_game_on:
+        time.sleep(0.1)
+        screen.update()
+        ball.move()
+
+        # Detect collision with wall
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.bounce_y()
+
+        # Detect collision with paddle
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+            ball.bounce_x()
+
+        # Detect if right paddle misses
+        if ball.xcor() > 380:
+            ball.reset_position()
+            score.l_point()
+
+        # Detect if left paddle misses
+        if ball.xcor() < -380:
+            ball.reset_position()
+            score.r_point()
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
